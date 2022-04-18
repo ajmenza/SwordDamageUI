205782b7505b754d2b6faa1bff40511271edb8ca
Tony Menza <ajm043@bucknell.edu>
2022-04-18 19:13:12 -0400

    Add project files.

---
+++ SwordDamageUI/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows;
+
+namespace SwordDamageUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random random = new Random();
+        SwordDamage swordDamage;
+        public MainWindow()
+        {
+            InitializeComponent();
+            swordDamage = new SwordDamage(random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7));
+            DisplayDamage();
+        }
+        public void RollDice()
+        {
+            swordDamage.Roll = random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
+            DisplayDamage();
+        }
+        private void DisplayDamage()
+        {
+            damage.Text = $"Rolled {swordDamage.Roll} for {swordDamage.Damage} HP";
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            RollDice();
+        }
+        private void Flaming_Checked(object sender, RoutedEventArgs e)
+        {
+            swordDamage.Flaming = true;
+            DisplayDamage();
+        }
+
+        private void Flaming_Unchecked(object sender, RoutedEventArgs e)
+        {
+            swordDamage.Flaming = false;
+            DisplayDamage();
+        }
+        private void Magic_Checked(object sender, RoutedEventArgs e)
+        {
+            swordDamage.Magic = true;
+            DisplayDamage();
+        }
+        private void Magic_Unchecked(object sender, RoutedEventArgs e)
+        {
+            swordDamage.Magic = false;
+            DisplayDamage();
+        }
+    }
+}
